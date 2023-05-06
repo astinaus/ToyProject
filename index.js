0ba81cd7501d9657f10931c93da400f7da96cce3
astinaus <slaayers0@gmail.com>
2023-05-06 17:07:41 +0900

    메인페이지 추가

---
+++ index.js
@@ -0,0 +1,34 @@
+const projectList = document.querySelector(".project-list");
+
+async function renderProject() {
+  const response = await fetch("./assets/project.json");
+  const data = await response.json();
+  const frag = document.createDocumentFragment();
+  data.projects.forEach((el) => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+    const a = document.createElement("a");
+
+    li.classList.add("project-item");
+
+    img.src = el.image;
+    img.alt = el.name;
+    h2.textContent = el.name;
+    p.textContent = el.description;
+    a.href = el.link;
+    a.target = "_blank";
+    a.textContent = "View Project";
+
+    li.appendChild(img);
+    li.appendChild(h2);
+    li.appendChild(p);
+    li.appendChild(a);
+
+    frag.appendChild(li);
+  });
+  projectList.appendChild(frag);
+}
+
+renderProject();
